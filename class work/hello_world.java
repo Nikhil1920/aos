cb993cb1cb33eb7cfc83bcaef2abcb276e21be2d
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2021-07-20 08:25:58 +0530

    hello world using method

--- class work/hello_world.java
+++ class work/hello_world.java
@@ -1,6 +1,10 @@
-
 class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        hello();
+    }
+
+    static void hello() {
+        System.out.println("Hello World from method");
     }
 }
